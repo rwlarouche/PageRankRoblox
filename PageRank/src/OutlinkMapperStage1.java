76f19b050ef787d1a0cc32248ad859ffea88d1c3
Ryan LaRouche <rwlarouche@yahoo.com>
2017-04-28 14:39:45 -0400

    Core upload
    Upload core files

---
+++ PageRank/src/OutlinkMapperStage1.java
@@ -0,0 +1,68 @@
+//package PageRank;
+
+import java.io.IOException;
+import java.nio.charset.CharacterCodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+
+public class OutlinkMapperStage1 extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+
+    private static final Pattern wikiLinksPattern = Pattern.compile("\\[.+?\\]");
+
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+
+        String page = value.toString();
+        String txt = "";
+        String title = "";
+        Set<String> set = new HashSet<String>();
+
+        title = page.substring(page.indexOf("<title>")+7, page.indexOf("</title>")).replace(" ", "_");
+
+        int startIndex = page.indexOf("<text");
+        int endIndex = page.indexOf("</text>");
+        if ( startIndex != -1 && endIndex != -1)
+            txt = page.substring(startIndex, endIndex );
+
+        Pattern p = Pattern.compile("\\[\\[(.*?)\\]\\]");
+        Matcher m = p.matcher(txt);
+
+        //This part handles the red links
+        output.collect(new Text(title), new Text("#"));
+
+        while(m.find()) {
+
+            String temp = m.group(1);
+
+//            if(temp != null & !temp.isEmpty() && temp.charAt(0) != '#' && temp.charAt(0) != ',' && temp.charAt(0) != '.' && temp.charAt(0) != '&'
+//                    && temp.charAt(0) != '\\' && temp.charAt(0) != '-' && temp.charAt(0) != '{'
+//                    && !temp.contains("&") && !temp.contains(":") && !temp.contains(","))
+//            {
+            if(temp != null && !temp.isEmpty())
+            {
+                if(temp.contains("|")){
+                    String outlink = temp.substring(0, temp.indexOf("|")).replace(" ", "_");
+                    if ( !outlink.equals(title))
+                        output.collect(new Text(outlink), new Text(title) );
+                }
+                else{
+                    String outlink = temp.replace(" ","_");
+                    if ( !outlink.equals(title))
+                        output.collect( new Text(outlink), new Text(title) );
+                }
+            }
+        }
+    }
+}
---
+++ PageRank/src/OutlinkReducerStage1.java
@@ -0,0 +1,54 @@
+//package PageRank;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class OutlinkReducerStage1 extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+
+    public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+
+        StringBuilder outlinks = new StringBuilder();
+        Set<String> set = new HashSet<String>();
+        int count = 0 ;
+        Boolean isRedLink = true;
+
+        while ( values.hasNext()){
+            //Add only unique links
+            set.add(values.next().toString());
+            count++;
+        }
+
+//        if ( set.size() == 1){
+//            //if the page is a sink
+//            if (set.contains("#")){
+//                output.collect(key, new Text("#"));
+//            }
+//
+//        }else{
+
+            if (set.contains("#")){
+                Iterator i = set.iterator();
+                while ( i.hasNext()){
+                    String val = (String) i.next();
+                    if ( !val.equals("#"))
+                        output.collect(new Text(val), key);
+                    else {
+                        output.collect( key, new Text("#"));
+                    }
+                }
+            }
+
+        //}
+    }
+}
